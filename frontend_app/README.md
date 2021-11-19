b4d4a68057da9c601e923e58c8d59d4ede53defa
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-11-19 16:14:42 -0300

    front end

--- frontend_app/README.md
+++ frontend_app/README.md
@@ -1,4 +1,4 @@
-# frontend
+# frontend_app
 
 A new Flutter project.
 
--- frontend_app/android/app/src/main/kotlin/com/example/frontend_app/MainActivity.kt
+++ frontend_app/android/app/src/main/kotlin/com/example/frontend_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.frontend
+package com.example.frontend_app
 
 import io.flutter.embedding.android.FlutterActivity
 
