00baed51c3b42bbbdaecc2c5517619107c3bd28c
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-11-22 17:51:25 -0300

    Iniciando a tela de criação de post 5:50 minutos

--- frontend_app/lib/screens/post_form.dart
+++ frontend_app/lib/screens/post_form.dart
@@ -10,6 +10,28 @@ class PostForm extends StatefulWidget {
 class _PostFormState extends State<PostForm> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Adicione um novo Post'),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Center(
+              child: IconButton(
+                icon: Icon(
+                  Icons.image,
+                  size: 50,
+                  color: Colors.black38,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
