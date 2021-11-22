8472bb1285c6f9ee49b7169438da3fdaa883d41a
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-11-22 16:44:23 -0300

    Logout

--- frontend_app/lib/screens/home.dart
+++ frontend_app/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_app/screens/login.dart';
+import 'package:frontend_app/services/user_service.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,7 +14,16 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: GestureDetector(
+            child: GestureDetector(
+                onTap: () {
+                  logout().then((value) => {
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) => Login()),
+                            (route) => false)
+                      });
+                },
+                child: Text('Press to Logout'))),
       ),
     );
   }
