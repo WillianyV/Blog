5be1a2a658714dbc1d3267598823b1a16cd34292
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-11-23 11:50:20 -0300

    Iniciando tela do post, já pegando informações, agora só falta estilizar

--- frontend_app/lib/screens/post_screen.dart
+++ frontend_app/lib/screens/post_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_app/constant.dart';
+import 'package:frontend_app/models/api_response.dart';
+import 'package:frontend_app/models/post.dart';
+import 'package:frontend_app/screens/login.dart';
+import 'package:frontend_app/services/post_service.dart';
+import 'package:frontend_app/services/user_service.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({Key? key}) : super(key: key);
@@ -8,12 +14,47 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  //variaveis
+  List<dynamic> _postList = [];
+  int userId = 0;
+  bool _loading = true;
+  //metodos
+  //pegar todos os posts
+  Future<void> retrievePosts() async {
+    userId = await getUseId();
+    ApiResponse response = await getPosts();
+    if (response.error == null) {
+      setState(() {
+        _postList = response.data as List<dynamic>;
+        _loading = _loading ? !_loading : _loading;
+      });
+    } else if (response.error == unauthorized) {
+      logout().then((value) => {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => Login()),
+                (route) => false)
+          });
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('${response.error}')));
+    }
+  }
+
+  @override
+  void initState() {
+    retrievePosts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Posts'),
-      ),
-    );
+    return _loading
+        ? Center(child: CircularProgressIndicator())
+        : ListView.builder(
+            itemCount: _postList.length,
+            itemBuilder: (BuildContext context, int index) {
+              Post post = _postList[index];
+              return Text('${post.body}');
+            });
   }
 }
